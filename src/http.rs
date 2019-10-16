47a5559983a23a3717fce2d3ed89db3041fef8a6
Clay Raynor <clay.raynor@stratifyd.com>
2019-10-16 17:24:17 -0400

    Add Code Level Documentation
    Documentation comments and general comments were added to the http.rs
    module.

--- src/http.rs
+++ src/http.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use std::error::Error;
 
+/// Represents a parsed incoming HTTP request
 pub struct HttpRequest<'a>
 {
     http_method: &'a str,
@@ -9,6 +10,19 @@ pub struct HttpRequest<'a>
     body: Option<&'a str>,
 }
 
+/// Parse a HTTP request
+///
+/// # Parameters
+///
+/// - `request`: a reference to the `str` of data to parse as an HTTP request
+///
+/// # Returns
+///
+/// A `Result` which is:
+///
+/// - `OK`: A `HttpRequest` struct containing the information parsed from the HTTP request
+/// - `Box`: Returns an error encapsulated in a `Box`.
+/// TODO: replace the boxed error with an enum of possible error types.
 pub fn parse_request(request: &str) -> Result<HttpRequest, Box<dyn Error>>
 {
     // Break the request line up into its different components
@@ -23,6 +37,8 @@ pub fn parse_request(request: &str) -> Result<HttpRequest, Box<dyn Error>>
     {
         "GET" | "HEAD" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" => (),
         "POST" | "PUT" | "PATCH" => {
+            // If the request's method should have a body, find the start of the body with
+            // as indicated with the CRLF.
             let body_start = match request.find("\r\n")
             {
                 Some(i) => i,
@@ -30,12 +46,14 @@ pub fn parse_request(request: &str) -> Result<HttpRequest, Box<dyn Error>>
             };
             body = Some(&request[body_start..]);
         },
+        // Return an error for any invalid method.
         _ => Err("Unsupported method!")?,
     }
 
     let uri = Path::new(parts.next().ok_or("URI not specified")?);
     let http_version = parts.next().ok_or("HTTP version not specified")?;
 
+    // Return an error for any requests that aren't HTTP/1.1
     if http_version != "HTTP/1.1"
     {
         Err("Only HTTP/1.1 is supported!")?;
@@ -59,14 +77,17 @@ mod tests
     use std::path::Path;
 
     #[test]
+    /// Verify that the `parse_request()` function correctly parses a valid HTTP GET request
+    /// by returning a `Request` struct containing the HTTP request's details.
     fn test_parse_request_get_pos()
     {
-        let get_request = "GET /some/path/ HTTP/1.1\n";
+        // Test the parsing of a simple GET request with no headers.
+        let get_request = "GET / HTTP/1.1\r\n";
 
         let result = parse_request(get_request).unwrap();
         let expected_result = HttpRequest {
             http_method: "GET",
-            uri: Path::new("/some/path/"),
+            uri: Path::new("/"),
             http_version: "HTTP/1.1",
             body: None,
         };
@@ -75,12 +96,32 @@ mod tests
         assert_eq!(result.http_version, expected_result.http_version);
         assert_eq!(result.body, expected_result.body);
 
-        let get_request = "GET / HTTP/1.1\nHost: www.example.com\nConnection: keep-alive\n";
+        let get_request =
+        "GET / HTTP/1.1
+        Host: www.example.com
+        Connection: keep-alive\r\n";
 
         let result = parse_request(get_request).unwrap();
         let expected_result = HttpRequest {
             http_method: "GET",
             uri: Path::new("/"),
+            http_version: "HTTP/1.1",
+            body: None,
+        };
+        assert_eq!(result.http_method, expected_result.http_method);
+        assert_eq!(result.uri, expected_result.uri);
+        assert_eq!(result.http_version, expected_result.http_version);
+        assert_eq!(result.body, expected_result.body);
+
+        let get_request =
+        "GET /some/path/ HTTP/1.1
+        Host: www.example.com
+        Connection: keep-alive\r\n";
+
+        let result = parse_request(get_request).unwrap();
+        let expected_result = HttpRequest {
+            http_method: "GET",
+            uri: Path::new("/some/path"),
             http_version: "HTTP/1.1",
             body: None,
         };
