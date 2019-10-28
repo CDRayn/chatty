8806806c3e5b3d7fc616bd7cbf89ba1f8be15eb6
Clay Raynor <clay.raynor@stratifyd.com>
2019-10-28 12:29:20 -0400

    Add parse_chat() Function
    The parse_chat() function was added to parse a JSON formatted chat
    object from a &str.

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,8 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{Result, from_str};
+#![allow(non_snake_case)]
 use std::error::Error;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
 /// # Chat Struct
 ///
@@ -10,8 +12,9 @@ use std::error::Error;
 #[derive(Serialize, Deserialize)]
 pub struct Chat
 {
-    id: u32,
-    participant_ids: [u32; 2],
+    #[serde(default)]
+    pub id: Option<u32>,
+    pub participantIds: [u32; 2],
 }
 
 /// # Message Struct
@@ -24,16 +27,64 @@ pub struct Chat
 #[derive(Serialize, Deserialize)]
 pub struct Message<'a>
 {
-    source_user_id: u32,
-    destination_user_id: u32,
-    timestamp: u32,
-    message: &'a str,
+    pub sourceUserId: u32,
+    pub destinationUserId: u32,
+    pub timestamp: u32,
+    pub message: &'a str,
 }
 
 /// Parses an HTTP body and returns a `Chat` struct containing the parsed contents
 /// of the HTTP body.
 pub fn parse_chat(http_body: &str) -> Result<Chat>
 {
-    let chat: Chat = serde_json::from_str(http_body)?;
-    return Ok(chat)
+
+
+    let chat= serde_json::from_str(http_body);
+    return chat;
+}
+
+#[cfg(test)]
+mod test
+{
+    use super::*;
+
+    /// Verify that the `parse_chat()` function correctly parses a `Chat` struct from
+    /// a JSON formatted HTTP body.
+    #[test]
+    fn test_parse_chat_valid()
+    {
+        // Test the parsing of a JSON formatted chat object containing all fields
+        let mut json_chat = r#"
+            {
+                "id": 34,
+                "participantIds": [3423, 9813]
+            }
+        "#;
+        let mut expected = Chat {
+            id: Some(34),
+            participantIds: [3423, 9813],
+        };
+        let mut parsed_chat = parse_chat(&json_chat).unwrap();
+
+        assert_eq!(expected.id, parsed_chat.id);
+        assert_eq!(expected.participantIds[0], parsed_chat.participantIds[0]);
+        assert_eq!(expected.participantIds[1], parsed_chat.participantIds[1]);
+
+        // Test the parsing of a JSON formatted chat object that does
+        // not contain the id field.
+        json_chat = r#"
+            {
+                "participantIds": [3423, 9813]
+            }
+        "#;
+        expected = Chat {
+            id: None,
+            participantIds: [3423, 9813],
+        };
+        parsed_chat = parse_chat(&json_chat).unwrap();
+
+        assert_eq!(expected.id, parsed_chat.id);
+        assert_eq!(expected.participantIds[0], parsed_chat.participantIds[0]);
+        assert_eq!(expected.participantIds[1], parsed_chat.participantIds[1]);
+    }
 }
